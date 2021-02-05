d6784e102c27911c741cbf83116d79c440c14c76
Ananta98 <ananta96@gmail.com>
2021-02-04 23:09:31 -0800

    modified:   src/main.rs 	modified:   src/mm/frame.rs 	modified:   src/mm/heap.rs

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ mod mm;
 use core::{panic::PanicInfo};
 use bootloader::{BootInfo, entry_point};
 use x86_64::VirtAddr;
-use mm::{frame::BootInfoFrameAllocator, heap};
+use mm::heap;
 
 #[macro_use]
 mod drivers;
@@ -41,7 +41,6 @@ fn kernel_main(boot_info : &'static BootInfo) -> ! {
     x86_64::instructions::interrupts::enable();
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { mm::initialize_paging(phys_mem_offset)};
-    let mut boot_frame_allocator= BootInfoFrameAllocator::initialize_frame_allocator(&boot_info.memory_map);
-    heap::initialize_heap(&mut mapper,&mut boot_frame_allocator).expect("Heap initialization failed");
+    heap::initialize_heap(&mut mapper,&mut boot_info.memory_map).expect("Heap initialization failed");
     hlt_loop();
 }
