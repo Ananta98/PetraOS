440a2c0ee558d3ce119f559c531fe48ac9442177
Ananta <kusumaananta042@gmail.com>
2026-08-08 11:33:57 +0700

    chore: remove unused fd

--- kernel/src/fs/fd/mod.rs
+++ kernel/src/fs/fd/mod.rs
@@ -1,11 +1,3 @@
-pub mod epoll;
-pub mod eventfd;
 pub mod fd_table;
-pub mod inotify;
-pub mod signalfd;
 
-pub use epoll::EpollFd;
-pub use eventfd::EventFd;
 pub use fd_table::FdTable;
-pub use inotify::InotifyFd;
-pub use signalfd::SignalFd;
